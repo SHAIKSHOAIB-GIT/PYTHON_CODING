e7ec438ffbb403f08a4c2c876beb3b864ec4c1d0
SHAIKSHOAIB-GIT <shaikshoaib820@gmail.com>
2024-05-24 16:07:43 +0530

    adding list.py file

---
+++ DAY1/list.py
@@ -0,0 +1,17 @@
+# using list and index position
+# using emoji from browser
+
+row1 = ["🤖","🤖","🤖"]
+row2 = ["🤖","🤖","🤖"]
+row3 = ["🤖","🤖","🤖"]
+
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("where do you want to put the treasure?")
+
+rows = int(position[0])
+cols = int(position[1])
+
+map[rows-1][cols-1]='x'
+
+print(f"{row1}\n{row2}\n{row3}")
