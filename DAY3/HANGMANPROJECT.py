cbdb85c21cd32b577d9f091d62e1fcf0fd4ecb57
SHAIKSHOAIB-GIT <shaikshoaib820@gmail.com>
2024-05-26 14:53:46 +0530

    finalproject

--- DAY3/HANGMANPROJECT.py
+++ DAY3/HANGMANPROJECT.py
@@ -1,20 +1,118 @@
 import random
+stages = ['''
++---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''',
+ '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+logo = ''' 
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/    '''
 
 word_list = ["sam", "virat","dhoni","sten","bolt","samson","hyd"]
-chosse_word = random.choice(word_list)
-print(chosse_word)
-display=[]
-for letters in chosse_word:
-    display+='_'
-print(display)
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
 
-word_len = len(chosse_word)
-for given_letter in range(word_len):
-    given_letter= input("guess the letter : ").lower()
+end_of_game = False
+lives = 6
 
-    for position in range(len(chosse_word)):
-        letters = chosse_word[position]
-        if letters == given_letter:
-            display[position]=letters
+print(logo)
 
-print(display)                                     
+#Create blanks
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    #TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
+    if guess in display:
+        print(f"You've already guessed {guess}")
+
+    #Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        #print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
+
+    #Check if user is wrong.
+    if guess not in chosen_word:
+        
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
+        
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+
+    #Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+    #Check if user has got all letters.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+
+    
+    print(stages[lives])
+
